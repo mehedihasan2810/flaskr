7c7f2587a770dee3853f6f39a66773994358ef25
mehedi-hasan <mdmehedihasan2810@gmail.com>
2024-05-25 17:46:26 +0600

    fix: requirements.txt

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -6,10 +6,10 @@ import sqlite3
 def create_app(test_config=None):
     app = Flask(__name__,  instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY='dev',
+        SECRET_KEY=os.environ.get('SECRET_KEY', 'dev'),
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
     )
-       
+     
     if test_config is None:
         app.config.from_pyfile('config.py', silent=True)
     else:
--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from myapp import create_app
+from flaskr import create_app
 
 app = create_app()
 
